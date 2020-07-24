4036bbf5675b0e9b4ea4f37f26da22ea7d947a98
matsueushi <matsueushi@gmail.com>
2020-07-24 15:23:55 +0900

    Simplify maximum_filter

--- src/filters.jl
+++ src/filters.jl
@@ -1,14 +1,15 @@
 function maximum_filter(matrix, filter_size)
-    result = zero(matrix)
+    temp, result = zero(matrix), zero(matrix)
     n1, n2 = size(matrix)
     for i in 1:n1
-        for j in 1:n2
-            i_min = max(i - (filter_size - 1), 1)
-            i_max = min(i + (filter_size - 1), n1)
-            j_min = max(j - (filter_size - 1), 1)
-            j_max = min(j + (filter_size - 1), n2)
-            result[i, j] = maximum(view(matrix, i_min:i_max, j_min:j_max))
-        end
+        i_min = max(i - (filter_size - 1), 1)
+        i_max = min(i + (filter_size - 1), n1)
+        temp[i, :] = maximum(view(matrix, i_min:i_max, :), dims=1)
+    end
+    for j in 1:n2
+        j_min = max(j - (filter_size - 1), 1)
+        j_max = min(j + (filter_size - 1), n2)
+        result[:, j] = maximum(view(temp, :, j_min:j_max), dims=2)
     end
     return result
 end
