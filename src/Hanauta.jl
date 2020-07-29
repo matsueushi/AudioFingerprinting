c87644257dba54f09ab3d354598d9b4752fa8f27
matsueushi <matsueushi@gmail.com>
2020-07-30 00:23:32 +0900

    Fingerprint each channel

--- src/Hanauta.jl
+++ src/Hanauta.jl
@@ -1,12 +1,13 @@
 module Hanauta
 
+using DSP
 using SHA
 using Statistics
-using DSP
+using Plots
 
 include("peaks.jl")
 include("fingerprint.jl")
 
-export maxfilter, findpeaks, hashpeaks, songspectrogram, fingerprint
+export maxfilter, findpeaks, hashpeaks, fingerprint, fingerprint_song
 
 end # module
